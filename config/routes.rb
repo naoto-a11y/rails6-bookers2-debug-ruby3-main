e4ec6015d7c48edde3d285303040b96869dee923
EC2 Default User <ec2-user@ip-172-31-6-245.ap-northeast-1.compute.internal>
2025-03-20 16:29:04 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'relationships/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to =>"homes#top"
   devise_for :users
@@ -10,7 +11,11 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
   end
 
-  resources :users, only: [:index,:show,:edit,:update]
-  
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy] 
+      get :follows, on: :member
+      get :followers, on: :member
+  end
+   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
